940b8a8a4a199dfbea6777059f879394b44c2c24
Jakub Kopeć <s24530@pjwstk.edu.pl>
2025-01-10 10:16:55 +0100

    feat: Clean up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                        "https://github.com/s24530/TAPI.git/tree/main/packages/create-docusaurus/templates/shared/",
                 },
                 blog: {
                     showReadingTime: true,
@@ -57,7 +57,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                        "https://github.com/s24530/TAPI.git/tree/main/packages/create-docusaurus/templates/shared/",
                     // Useful options to enforce blogging best practices
                     onInlineTags: "warn",
                     onInlineAuthors: "warn",
@@ -76,7 +76,7 @@ const config = {
             // Replace with your project's social card
             image: "img/docusaurus-social-card.jpg",
             navbar: {
-                title: "My Site",
+                title: "My Space API Docs",
                 logo: {
                     alt: "My Site Logo",
                     src: "img/logo.svg",
@@ -90,7 +90,7 @@ const config = {
                     },
 
                     {
-                        href: "https://github.com/facebook/docusaurus",
+                        href: "https://github.com/s24530/TAPI.git",
                         label: "GitHub",
                         position: "right",
                     },
@@ -130,7 +130,7 @@ const config = {
                         items: [
                             {
                                 label: "GitHub",
-                                href: "https://github.com/facebook/docusaurus",
+                                href: "https://github.com/s24530/TAPI.git",
                             },
                         ],
                     },
